4a5563f9ccd17b1cfb63ffeb5c98bd5bc83c3d54
youssef <bonyoussef@gmail.com>
2020-09-27 18:15:25 +0200

    first add

---
+++ java-csv-file-handling-with-apache-commons-csv/src/main/java/CSVReaderWithHeaderAutoDetection.java
@@ -0,0 +1,44 @@
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * Created by rajeevkumarsingh on 25/09/17.
+ */
+public class CSVReaderWithHeaderAutoDetection {
+    private static final String SAMPLE_CSV_FILE_PATH = "C:\\Users\\pc_ycf\\Documents\\NetBeansProjects\\java-read-write-csv-file\\java-csv-file-handling-with-apache-commons-csv\\users-with-header.csv";
+
+    public static void main(String[] args) throws IOException {
+        try (
+            Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
+            CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT
+                    .withFirstRecordAsHeader()
+                    .withIgnoreHeaderCase()
+                    .withTrim());
+        ) {
+            for (CSVRecord csvRecord : csvParser) {
+                // Accessing values by Header names
+
+                String name = csvRecord.get("Name");
+                String email = csvRecord.get("Email");
+                String phone = csvRecord.get("Phone");
+                String country = csvRecord.get("Country");
+
+                System.out.println("Record No - " + csvRecord.getRecordNumber());
+                System.out.println("---------------");
+                System.out.println("Name : " + name);
+                System.out.println("Email : " + email);
+                System.out.println("Phone : " + phone);
+                System.out.println("Country : " + country);
+                System.out.println("---------------\n\n");
+            }
+        }
+    }
+
+}
